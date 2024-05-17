316fe14639c9a026e5b9032f58b0dd549da893d4
Roman Onishchenko <roman.onishchenko@onelog.ch>
2024-05-17 19:41:53 +0300

    implementation

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,22 @@
-class Storage {}
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
 
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
